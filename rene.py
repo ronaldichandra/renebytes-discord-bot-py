a2cd9d74557ef8409f599d3325c5b31b18e59d33
Ronaldi Chandra <ronhyun@protonmail.com>
2020-06-15 20:10:28 +0700

    Update rene.py

--- rene.py
+++ rene.py
@@ -4,9 +4,17 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix=".")
 TOKEN = os.getenv("TOKEN")
 
+async def status_task():
+    while True:
+        await test_bot.change_presence("follow ig @renebaebae")
+        await asyncio.sleep(10)
+        await test_bot.change_presence("sama ronhyun")
+        await asyncio.sleep(10)
+
 @bot.event
 async def on_ready():
     print("Masuk sebagai {}({})".format(bot.user.name, bot.user.id))
+    bot.loop.create_task(status_task())
 
 @bot.command()
 async def ping(ctx):
