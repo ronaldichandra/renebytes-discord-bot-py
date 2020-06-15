2ed43f38e5602169505b70b434bf9585ac7a86f6
Ronaldi Chandra <ronhyun@protonmail.com>
2020-06-15 21:51:33 +0700

    Update rene.py

--- rene.py
+++ rene.py
@@ -13,15 +13,16 @@ async def on_ready():
     print("Masuk sebagai {}({})".format(bot.user.name, bot.user.id))
     await bot.change_presence(activity=discord.Game(name="follow ig @renebaebae"))
 
+async def on_message(message):
+    if message.content.startswith('<@'):
+    await client.send_message(message.channel, "Ummm")
+
 @bot.command(pass_context=True)
 async def ping(ctx):
     em = discord.Embed(color=discord.Colour.magenta())
     em.title = "Pong!"
     em.description = f'{bot.latency * 1000} ms'
     await ctx.send(embed=em)
-
-if message.content.startswith('<@'):
-    await client.send_message(message.channel, "Ummm")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
