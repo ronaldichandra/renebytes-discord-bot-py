699cc5a6c4c198596ffda8f2c266688d88e8f5c7
Ronaldi Chandra <ronhyun@protonmail.com>
2020-06-15 19:43:28 +0700

    Add files via upload

---
+++ rene.py
@@ -0,0 +1,23 @@
+# Work with Python 3.6
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Masuk sebagai')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(process.env.TOKEN)
