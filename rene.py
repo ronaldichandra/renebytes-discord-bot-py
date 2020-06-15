81eaef993df261f2f38ca5accce206832d7ebcbd
Ronaldi Chandra <ronhyun@protonmail.com>
2020-06-15 20:29:43 +0700

    Update rene.py

--- rene.py
+++ rene.py
@@ -5,17 +5,11 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix=".")
 TOKEN = os.getenv("TOKEN")
 
-async def status_task():
-    while True:
-        await bot.change_presence(game=discord.Game(name='follow ig @renebaebae')
-        await asyncio.sleep(2)
-        await bot.change_presence(game=discord.Game(name='sama ronhyun')
-        await asyncio.sleep(2)
 
 @bot.event
 async def on_ready():
     print("Masuk sebagai {}({})".format(bot.user.name, bot.user.id))
-    bot.loop.create_task(status_task())
+    await bot.change_presence(activity=discord.Game(name="follow ig @renebaebae"))
 
 @bot.command()
 async def ping(ctx):
